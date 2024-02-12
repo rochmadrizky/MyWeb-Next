7099e9940843f5ddc2238b036e61713ea07916b4
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-02-12 09:38:13 +0700

    fix kan posisi footer

--- src/app/components/navigasi/FotBar.tsx
+++ src/app/components/navigasi/FotBar.tsx
@@ -17,7 +17,7 @@ const FotBar = () => {
   ];
 
   return (
-    <div className="max-w-7xl mx-auto p-6 text-base bottom-0">
+    <div className="max-w-7xl mx-auto p-2 text-base bottom-0">
       <div className="flex flex-col md:flex-row items-center  justify-center md:justify-between gap-y-1 px-4">
         <div className="flex items-center gap-2">
           {icons.map((barang, urutan) => (
