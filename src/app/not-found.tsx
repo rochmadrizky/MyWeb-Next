5e96432b62aef3c0c2f3d322e3825de08e696a88
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-02-16 17:52:49 +0700

    menambahkan halaman error ygy

---
+++ src/app/not-found.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="p-6 text-center">
+        <h1 className="font-prefix text-4xl">
+          The page doesn't exist, ya guys ya.
+        </h1>
+        <p className="py-1 font-description text-2xl">
+          please return to the homepage, ya guys ya.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
