87d2e742de4c0df7bca5d6885baccb70a4b2f49b
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-02-20 09:53:34 +0700

    nambahkan favicon gambar saya ygy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/me/thisIsMe.png" sizes="any" />
+      </head>
       <body className="bg-gray-200">
         <Navbar />
         <div className="min-h-screen">{children}</div>
