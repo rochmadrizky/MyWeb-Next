bd9abc7ed974f59952dc4fcac6a5106c33fa8d66
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-02-26 12:47:19 +0700

    update profile

--- src/app/components/home/HomeContent.tsx
+++ src/app/components/home/HomeContent.tsx
@@ -1,27 +1,13 @@
-"use client";
-
-import React, { useState } from "react";
-import ModalProfile from "./ModalProfile";
+import React from "react";
 
 const HomeContent = () => {
-  const [modalTerbuka, mengaturModalTerbuka] = useState(false);
-
-  const membukaModal = () => {
-    mengaturModalTerbuka(true);
-  };
-
-  const menutupModal = () => {
-    mengaturModalTerbuka(false);
-  };
-
   return (
     <div className="max-w-4xl mx-auto flex flex-col items-center justify-center">
       <div className="p-4">
         <img
           src="/me/thisIsMe.png"
           alt="saya"
-          className="rounded-full mx-auto w-36 h-36 mb-3 ring-2 p-0.5 ring-blue-500 bg-gray-300 cursor-pointer"
-          onClick={membukaModal}
+          className="rounded-full mx-auto w-36 h-36 mb-3 ring-2 p-0.5 ring-blue-500 bg-gray-300"
         />
       </div>
 
@@ -32,8 +18,6 @@ const HomeContent = () => {
         <h1 className="text-5xl font-title">Rizky Putra</h1>
         <p className="text-xl font-prefix">Front-End Developer</p>
       </div>
-
-      {modalTerbuka && <ModalProfile closeModal={menutupModal} />}
     </div>
   );
 };
