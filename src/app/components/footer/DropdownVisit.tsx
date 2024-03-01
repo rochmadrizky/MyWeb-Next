f3c998c535cafd10b8ff3162617d147e96f278d8
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-03-02 01:36:45 +0700

    update bang

--- src/app/components/footer/DropdownVisit.tsx
+++ src/app/components/footer/DropdownVisit.tsx
@@ -7,46 +7,49 @@ interface DropdownVisitProps {
 
 const DropdownVisit: React.FC<DropdownVisitProps> = ({ buka, tutup }) => {
   const dropdown = useRef<HTMLDivElement>(null);
-  const [hariBulan, mengaturHariBulan] = useState<string>("");
-  const [jamMenit, mengaturJamMenit] = useState<string>("");
+  const [hariBulan, setHariBulan] = useState<string>("");
+  const [jamMenit, setJamMenit] = useState<string>("");
 
   useEffect(() => {
-    const klikDiluar = (klik: MouseEvent) => {
-      if (dropdown.current && !dropdown.current.contains(klik.target as Node)) {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdown.current &&
+        !dropdown.current.contains(event.target as Node)
+      ) {
         tutup();
       }
     };
 
-    const klikEscape = (klik: KeyboardEvent) => {
-      if (klik.key === "Escape") {
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
         tutup();
       }
     };
 
     if (buka) {
-      document.addEventListener("mousedown", klikDiluar);
-      document.addEventListener("keydown", klikEscape);
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("keydown", handleEscapeKey);
       updateDateTime();
     } else {
-      document.removeEventListener("mousedown", klikDiluar);
-      document.removeEventListener("keydown", klikEscape);
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeKey);
     }
 
     return () => {
-      document.removeEventListener("mousedown", klikDiluar);
-      document.removeEventListener("keydown", klikEscape);
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeKey);
     };
   }, [buka, tutup]);
 
   const updateDateTime = () => {
     const date = new Date();
-    const jam = date.getHours();
-    const menit = date.getMinutes();
-    const tanggal = date.getDate();
-    const bulan = date.getMonth() + 1;
-    const tahun = date.getFullYear();
-    const hari = date.getDay();
-    const namaHari = [
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    const dayOfWeek = date.getDay();
+    const days = [
       "Sunday",
       "Monday",
       "Tuesday",
@@ -54,14 +57,13 @@ const DropdownVisit: React.FC<DropdownVisitProps> = ({ buka, tutup }) => {
       "Thursday",
       "Friday",
       "Saturday",
-    ][hari];
-
-    const namaBulan = [
+    ];
+    const months = [
       "January",
       "February",
       "March",
       "April",
-      "Mey",
+      "May",
       "June",
       "July",
       "August",
@@ -69,22 +71,31 @@ const DropdownVisit: React.FC<DropdownVisitProps> = ({ buka, tutup }) => {
       "October",
       "November",
       "December",
-    ][bulan - 1];
+    ];
 
-    const hariBulanString = `${namaHari}, ${tanggal} ${namaBulan} ${tahun}`;
-    const jamMenitString = `${jam}:${menit.toString().padStart(2, "0")}`;
+    const dayMonthString = `${days[dayOfWeek]}, ${day} ${
+      months[month - 1]
+    } ${year}`;
+    const hourMinuteString = `${hours}:${minutes.toString().padStart(2, "0")}`;
 
-    mengaturHariBulan(hariBulanString);
-    mengaturJamMenit(jamMenitString);
+    setHariBulan(dayMonthString);
+    setJamMenit(hourMinuteString);
   };
 
   return (
     <div
-      className={`fixed z-10 top-0 left-0 w-full h-full bg-black bg-opacity-70 flex items-center justify-center transition-opacity ${
+      className={`fixed top-2 left-0 w-full h-full flex items-center justify-center transition-opacity ${
         buka ? "opacity-100 duration-500" : "opacity-0 pointer-events-none"
       }`}
     >
-      <div ref={dropdown} className="p-8">
+      <div
+        ref={dropdown}
+        className={`p-8 absolute top-10 left-1/2 transform -translate-x-1/2 ${
+          buka
+            ? "translate-y-0 transition-transform duration-500"
+            : "-translate-y-full duration-500"
+        }`}
+      >
         <div className="p-2 border-2 border-blue-500 rounded-lg bg-gray-100 w-56 h-auto mx-auto">
           <div className="text-center">
             <p className="font-title text-md">{hariBulan}</p>
