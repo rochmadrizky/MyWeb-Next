d838f6d0b91aa53a67a84227aca2464f1cf1a7ea
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-03-08 01:32:48 +0700

    WIP ygy

--- src/app/(pages)/calculator/page.tsx
+++ src/app/(pages)/calculator/page.tsx
@@ -12,11 +12,9 @@ const page = () => {
           <div className="p-4">
             <div className="text-center">
               <h1 className="font-prefix text-2xl">
-                A collection of my blog notes.
+                Simple calculator that I made.
               </h1>
-              <p className="font-description text-xl">
-                This is where all my experiments are recorded.
-              </p>
+              <p className="font-description text-xl">Below is an example.</p>
             </div>
           </div>
         </div>
--- src/app/components/calculator/Calculator.tsx
+++ src/app/components/calculator/Calculator.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { IconBackspace } from "@tabler/icons-react";
 import React, { useState } from "react";
 
 const Calculator: React.FC = () => {
@@ -39,62 +40,95 @@ const Calculator: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col max-w-xs mx-auto">
-      <div className="w-full h-20 border border-black mb-2">
-        <div className=" text-right p-4">
-          <p>{ekspresi}</p>
-          <p>{hasil}</p>
+    <div>
+      <div className="flex flex-col max-w-xs mx-auto">
+        <div className="max-w-md border-b-2 border-t-2 border-blue-500 rounded-xl mb-2">
+          <div className="text-right p-4">
+            <p className="font-prefix text-lg">{ekspresi}</p>
+            <p className="font-prefix text-xl">{hasil}</p>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-center gap-2 mb-2">
+          {["C"].map((nilai, index) => (
+            <button
+              key={index}
+              className="bg-gray-100 text-xl p-4 hover:bg-gray-300 w-full border-l-2 border-r-2 border-blue-500 rounded-xl font-prefix"
+              onClick={() => menanganiKlik(nilai)}
+            >
+              {nilai}
+            </button>
+          ))}
+
+          <button
+            className="bg-gray-100 p-4 hover:bg-gray-300 w-full flex items-center justify-center border-l-2 border-r-2 border-blue-500 rounded-xl"
+            onClick={() => menanganiKlik("DEL")}
+          >
+            <IconBackspace />
+          </button>
+
+          {["="].map((nilai, index) => (
+            <button
+              key={index}
+              className="bg-gray-100 text-xl p-4 hover:bg-gray-300 w-full border-l-2 border-r-2 border-blue-500 rounded-xl font-prefix"
+              onClick={() => menanganiKlik(nilai)}
+            >
+              {nilai}
+            </button>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-4 gap-2">
+          {["7", "8", "9", "÷"].map((nilai, index) => (
+            <button
+              key={index}
+              className="bg-gray-100 text-xl p-4 hover:bg-gray-300 border-l-2 border-r-2 border-blue-500 rounded-xl font-prefix"
+              onClick={() => menanganiKlik(nilai)}
+            >
+              {nilai}
+            </button>
+          ))}
+
+          {["4", "5", "6", "×"].map((nilai, index) => (
+            <button
+              key={index}
+              className="bg-gray-100 text-xl p-4 hover:bg-gray-300 border-l-2 border-r-2 border-blue-500 rounded-xl font-prefix"
+              onClick={() => menanganiKlik(nilai)}
+            >
+              {nilai}
+            </button>
+          ))}
+
+          {["1", "2", "3", "+"].map((nilai, index) => (
+            <button
+              key={index}
+              className="bg-gray-100 text-xl p-4 hover:bg-gray-300 border-l-2 border-r-2 border-blue-500 rounded-xl font-prefix"
+              onClick={() => menanganiKlik(nilai)}
+            >
+              {nilai}
+            </button>
+          ))}
+
+          {["0", "-"].map((nilai, index) => (
+            <button
+              key={index}
+              className="bg-gray-100 text-xl p-4 hover:bg-gray-300 border-l-2 border-r-2 border-blue-500 rounded-xl font-prefix"
+              onClick={() => menanganiKlik(nilai)}
+            >
+              {nilai}
+            </button>
+          ))}
         </div>
       </div>
-      <div className="grid grid-cols-4 gap-2">
-        {["7", "8", "9", "÷"].map((nilai, index) => (
-          <button
-            key={index}
-            className="bg-gray-300 text-xl p-4 hover:bg-gray-400"
-            onClick={() => menanganiKlik(nilai)}
-          >
-            {nilai}
-          </button>
-        ))}
 
-        {["4", "5", "6", "×"].map((nilai, index) => (
-          <button
-            key={index}
-            className="bg-gray-300 text-xl p-4 hover:bg-gray-400"
-            onClick={() => menanganiKlik(nilai)}
-          >
-            {nilai}
-          </button>
-        ))}
-
-        {["1", "2", "3", "+"].map((nilai, index) => (
-          <button
-            key={index}
-            className="bg-gray-300 text-xl p-4 hover:bg-gray-400"
-            onClick={() => menanganiKlik(nilai)}
-          >
-            {nilai}
-          </button>
-        ))}
-
-        <button
-          className="bg-gray-300 text-xl p-4 hover:bg-gray-400"
-          onClick={() => menanganiKlik("DEL")}
-        >
-          DEL
-        </button>
-
-        {["C", "0", "=", "-"].map((nilai, index) => (
-          <button
-            key={index}
-            className={`bg-${
-              nilai === "=" ? "green" : "gray"
-            }-300 text-xl p-4 hover:bg-${nilai === "=" ? "green" : "gray"}-400`}
-            onClick={() => menanganiKlik(nilai)}
-          >
-            {nilai}
-          </button>
-        ))}
+      <div className="flex items-center justify-center">
+        <div className="p-8">
+          <h1 className="font-prefix text-2xl">Notes:</h1>
+          <p className="font-description">
+            I designed and made it with a simple appearance.
+          </p>
+          <p className="font-description">You can also try it..</p>
+        </div>
       </div>
     </div>
   );
