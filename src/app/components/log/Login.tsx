b482ab3330df9b9875567b2936c309f3f4791109
Rochmad Rizky Putra <rochmadrizkyputra@gmail.com>
2024-03-21 03:22:49 +0700

    WIP

--- src/app/components/log/Login.tsx
+++ src/app/components/log/Login.tsx
@@ -15,7 +15,9 @@ const Login = () => {
       setErrorMessage("");
     } else {
       setLoginSuccess(false);
-      if (email !== "try@example.com") {
+      if (email !== "try@example.com" && password !== "trylogin") {
+        setErrorMessage("Email dan Password salah, harap cek kembali");
+      } else if (email !== "try@example.com") {
         setErrorMessage("Email salah");
       } else {
         setErrorMessage("Password salah");
@@ -36,17 +38,23 @@ const Login = () => {
   return (
     <div className="flex items-center justify-center">
       <div className="flex flex-col md:flex-row gap-6 items-center justify-center rounded-2xl bg-gray-100 p-4">
-        <div className="w-80 flex justify-center">
-          <img
-            src="background/topography.svg"
-            alt="Login"
-            className="w-full h-full"
-          />
+        <div
+          className="w-80 h-80 flex items-center justify-center"
+          style={{ backgroundImage: `url('/background/topography.svg')` }}
+        >
+          <div className="bg-black bg-opacity-80 m-4 rounded-2xl">
+            <div className="p-4 text-center text-white">
+              <h1 className="font-prefix text-2xl pb-2">Log in account?</h1>
+              <p className="font-description">
+                Please enter the email and password provided in the notes.
+              </p>
+            </div>
+          </div>
         </div>
 
         <div className="w-80 border-black border rounded-2xl p-4">
           <div className="p-4">
-            <h2 className="text-center text-3xl font-extrabold text-gray-900">
+            <h2 className="text-center text-3xl font-title text-gray-900">
               {loginSuccess ? "Login Berhasil" : "Sign in to your account"}
             </h2>
           </div>
@@ -54,7 +62,7 @@ const Login = () => {
           {loginSuccess ? (
             <button
               onClick={handleLogout}
-              className="w-full flex justify-center py-2 px-4 text-sm font-medium rounded-md text-white bg-red-500"
+              className="w-full flex justify-center py-2 px-4 text-sm font-medium rounded-md text-white bg-red-500 font-prefix"
             >
               Logout
             </button>
@@ -72,7 +80,7 @@ const Login = () => {
                   type="email"
                   autoComplete="email"
                   required
-                  className="relative block w-full p-3 border border-gray-300 rounded-md focus:ring-blue-500 pr-10 bg-gray-200"
+                  className="relative block w-full p-3 border border-gray-300 rounded-md focus:ring-blue-500 pr-10 bg-gray-200 font-prefix"
                   placeholder="Email address"
                   value={email}
                   onChange={(e) => {
@@ -92,7 +100,7 @@ const Login = () => {
                   type={showPassword ? "text" : "password"}
                   autoComplete="current-password"
                   required
-                  className="relative block w-full p-3 border border-gray-300 rounded-md focus:ring-blue-500 pr-10 bg-gray-200"
+                  className="relative block w-full p-3 border border-gray-300 rounded-md focus:ring-blue-500 pr-10 bg-gray-200 font-prefix"
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
@@ -113,7 +121,7 @@ const Login = () => {
               <div>
                 <button
                   type="submit"
-                  className="w-full flex justify-center py-2 px-4 text-sm font-medium rounded-md text-white bg-blue-500"
+                  className="w-full flex justify-center py-2 px-4 text-sm font-medium rounded-md text-white bg-blue-500 font-prefix"
                   onClick={handleLogin}
                 >
                   Sign in
